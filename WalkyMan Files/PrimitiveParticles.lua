3df481f49664e29905a6ec34056b2079c0a40430
TheWinProgrammers <lucasburdell@hotmail.com>
2013-05-12 19:15:50 -0500

    Fixed Particles

--- WalkyMan Files/PrimitiveParticles.lua
+++ WalkyMan Files/PrimitiveParticles.lua
@@ -35,7 +35,18 @@ function newPrimitiveParticle(settings, screen)
 				['Boundaries'] = function(object) return not (object.Position[1]>screen.width or object.Position[1]<0 or object.Position[2]>screen.height or object.Position[2]<0) end,
 			}, screen)
 			drop:setLifespan(1)
-			drop
+			drop:setUpdate(function(self, dt)
+				if self.Update_Time==nil then
+					self.Update_Time = 0
+				end
+				self.Update_Time = self.Update_Time + dt
+				if self.Update_Time>.1 then
+					self.Transparency = self.Transparency + .1
+				end
+				if self.Transparency>1 then
+					self:Destroy()
+				end
+			end)
 			local imp = math.random(-5, 5)
 			imp = imp==0 and 1 or imp
 			drop.Physics.body:applyLinearImpulse(math.random(-1, 1)/10, 0)
